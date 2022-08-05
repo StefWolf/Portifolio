1bb7e1f10362f78ed5835edc3821d98c7856f8a3
StefWolf <stefaneassisori@gmail.com>
2022-08-05 21:42:00 +0100

    Criação de dark mode and light mode ainda não funcional

---
+++ src/Hooks/useTheme.jsx
@@ -0,0 +1,23 @@
+import {useContext, createContext, useState, useEffect} from "react";
+
+const ThemeContext = createContext();
+
+export default function ThemeContextProvider({children}){
+    const [theme, setTheme] = useState(localStorage.getItem("theme") !== "dark" ? "ligth" : "dark");
+
+    useEffect(()=>{
+        const root = window.document.documentElement;
+
+        root.classList.add(theme);
+        localStorage.setItem("theme", theme);
+    },[theme])
+    return(
+        <ThemeContext.Provider value={{theme, setTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export function useTheme(){
+    return useContext(ThemeContext);
+}
--- src/main.jsx
+++ src/main.jsx
@@ -9,20 +9,23 @@ import Blog from './Pages/Blog'
 import Projects from  './Pages/Projects'
 import Caderno from './Pages/SiteCaderno/Caderno'
 import Skills from './Pages/Skills'
+import ThemeContextProvider from './Hooks/useTheme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-        <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/blog/:blogURL" element={<Blog />} />
-            <Route path="/Projects" element={<Projects />} />
-            <Route path="/Caderno" element={<Caderno />} />
-            <Route path="/Skills" element={<Skills />} />
-          </Routes>
-        </BrowserRouter>
-    </ApolloProvider>
+    <ThemeContextProvider>
+      <ApolloProvider client={client}>
+          <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/blog/:blogURL" element={<Blog />} />
+              <Route path="/Projects" element={<Projects />} />
+              <Route path="/Caderno" element={<Caderno />} />
+              <Route path="/Skills" element={<Skills />} />
+            </Routes>
+          </BrowserRouter>
+      </ApolloProvider>
+    </ThemeContextProvider>
   </React.StrictMode>
 
 )
