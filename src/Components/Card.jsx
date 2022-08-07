ae77e5f618f5b568d4093bc5ad3645efa1a1c950
StefWolf <stefaneassisori@gmail.com>
2022-08-07 11:32:33 +0100

    Versão ligth mode do card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -11,16 +11,16 @@ export default function Card({img_url, url, descricao, title, icons, url_extern,
     }
     return(
         <>
-            <div data-aos="flip-left" className=" mb-3 lg:w-[20%] w-[80%] min-h-[300px] lg:h-[300px] rounded-md bg-Fundo-Secundario flex flex-col opacity-75 shadow-md hover:opacity-100 transition ml-10">
+            <div data-aos="flip-left" className=" mb-3 lg:w-[20%] w-[80%] min-h-[300px] lg:h-[300px] rounded-md dark:bg-Fundo-Secundario bg-Cor-Texto flex flex-col opacity-75 shadow-md hover:opacity-100 transition ml-10">
                 <div>
                     <img className="rounded-t-md" src={img_url} />
                 </div>
                 <div className="w-full  bg-[#303030]/60 justify-center items-center pl-2 pr-2 shadow-sm text-[11px]">
                    {statusProj()}
                 </div>
-                <div className="p-2 pl-4 gap-4">
-                    <h1 className="text-Cor-Texto font-bold text-lg">{title}</h1>
-                    <p className="text-Cor-Texto text-[14px]">{descricao}</p>
+                <div className="p-2 pl-4 gap-4 dark:text-Cor-Texto text-Fundo-dark">
+                    <h1 className=" font-bold text-lg">{title}</h1>
+                    <p className="text-[14px]">{descricao}</p>
                     <div className='flex gap-3 mt-2'>
                         <ButtonDark url = {url} text = "Read more" type="Dark" />
                         <ButtonDark url = {url_extern} text = "Repository" type="Dark" />
