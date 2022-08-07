aa22ac26cd546aed364fa9181dbecc02e6e40a5c
StefWolf <stefaneassisori@gmail.com>
2022-08-07 12:25:33 +0100

    Modificando hover do ButtonAll (Mais projetos...)

--- src/Components/Buttons/ButtonAll.jsx
+++ src/Components/Buttons/ButtonAll.jsx
@@ -1,7 +1,7 @@
 
 export default function ButtonAll(){
     return(
-        <button className="border-2 border-purple-800 transition-all  p-4 hover:border-purple-600 hover:text-purple-600 rounded-md text-purple-800">
+        <button className="border-2 border-purple-800 transition-all  p-4  hover:bg-purple-600 hover:border-Cor-Secundaria hover:text-Cor-Texto rounded-md text-purple-800">
             <a href="/Projects/">Mais projetos...</a>
         </button>
 
